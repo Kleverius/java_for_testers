ac9f055b0609a1fd10523832f9ee9630637031ed
Kobzoleg <kobzoleg@ya.ru>
2024-01-11 16:37:43 +0300

    ДЗ №18 - сценарий регистрации нового пользователя в MantisBT

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
@@ -18,4 +18,19 @@ public class SessionHelper extends HelperBase {
     public boolean isLoggedIn() {
         return isElementPresent(By.cssSelector("span.user-info"));
     }
+
+    public void register(String user, String email) {
+        click(By.linkText("Signup for a new account"));
+        type(By.name("username"), user);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[type='submit']"));
+        click(By.linkText("Proceed"));
+    }
+
+    public void setNewPassword(String realName, String password) {
+        type(By.name("realname"), realName);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
+    }
 }
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -0,0 +1,32 @@
+package ru.stqa.mantis.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.stqa.mantis.common.CommonFunctions;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+
+public class UserRegistrationTests extends TestBase {
+
+    @Test
+    void canRegisterUser() {
+        var username = CommonFunctions.randomString(8);
+        var password = "password";
+        var email = String.format("%s@localhost", username);
+        app.jamesCli().addUser(email, password);
+        app.session().register(username, email);
+        var messages = app.mail().receive(email, password, Duration.ofSeconds(10));
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S*");
+        var matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            var url = text.substring(matcher.start(), matcher.end());
+            app.driver().get(url);
+        }
+        var realName = "My_real_name";
+        app.session().setNewPassword(realName, password);
+        app.http().login(username, password);
+        Assertions.assertTrue(app.http().isLoggedIn());
+    }
+}
